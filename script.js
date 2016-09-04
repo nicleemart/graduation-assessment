bbcb94f89dc252e2ade62b510eab083ce86f5092
Nikki <nicleemart@gmail.com>
2016-09-03 23:07:44 -0500

    like/unlike works

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", function () {
-	
+	var like = document.getElementsByClassName("like_target");
+
+	for (i=0; i < like.length; i++){
+		like[i].addEventListener("click", function() {
+			if(this.innerHTML === "Like"){
+				this.innerHTML = "Unlike";
+			} else{
+				this.innerHTML = "Like";
+			}
+		});
+	};
+
 });
