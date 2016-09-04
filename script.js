ee55661762d310005312fce22d1fe7e489e7cbe4
Nikki <nicleemart@gmail.com>
2016-09-04 11:10:43 -0500

    like/unlike counter works

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 window.addEventListener("load", function () {
 	var like = document.getElementsByClassName("likeTarget");
-	var numberOfLikes = document.getElementsByClassName("likeNum");
+	var numberOfLikesElem = document.getElementsByClassName("likeNum");
 
 	for (i=0; i < like.length; i++){
-		like[i].addEventListener("click", function() {
-			if(this.innerHTML === "Like"){
-				this.innerHTML = "Unlike";
-			} else{
-				this.innerHTML = "Like";
-			}
-		});
+		like[i].addEventListener("click", likeFeature.bind(null, i));
 	};
+
+	function likeFeature(index){
+		var numberOfLikes = numberOfLikesElem[index].innerHTML;
+
+		if (like[index].innerHTML === "Like"){
+				like[index].innerHTML = "Unlike";
+				numberOfLikes++;
+				numberOfLikesElem[index].innerHTML = numberOfLikes;
+
+		} else{
+				like[index].innerHTML = "Like";
+				numberOfLikes--;
+				numberOfLikesElem[index].innerHTML = numberOfLikes;
+		}
+	}
 
 });
