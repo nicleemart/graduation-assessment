b5836a560f0c876125f098bc7d5a031b55289627
Nikki <nicleemart@gmail.com>
2016-09-04 12:34:29 -0500

    added comments and changed var names

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", function () {
-	var like = document.getElementsByClassName("likeTarget");
-	var numberOfLikesElem = document.getElementsByClassName("likeNum");
+	var like = document.getElementsByClassName("commentActions__likeTarget");
+	var numberOfLikesElem = document.getElementsByClassName("commentActions__likeNum");
 
 	for (i=0; i < like.length; i++){
 		like[i].addEventListener("click", likeFeature.bind(null, i));
+		// Loop through like class when clicked, use bind to attach function and param to click event
 	};
 
 	function likeFeature(index){
+		// Use index variable to indicate position in array
 		var numberOfLikes = numberOfLikesElem[index].innerHTML;
 
 		if (like[index].innerHTML === "Like"){
